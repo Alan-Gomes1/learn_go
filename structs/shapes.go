7cc5e04f42526fdbb60ddfc6e7c256702537cb92
Alan <alan.gomes.ag28@gmail.com>
2025-02-01 16:51:28 -0300

    refact: adding Triangle shape and refactoring tests

--- structs/shapes.go
+++ structs/shapes.go
@@ -23,10 +23,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Triangle struct {
+	Base float64
+	Height float64
 }
 
-func Area(Rectangle Rectangle) float64 {
-	return Rectangle.Width * Rectangle.Height
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
